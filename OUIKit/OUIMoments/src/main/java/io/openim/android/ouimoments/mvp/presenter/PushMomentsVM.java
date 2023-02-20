1851d0e94abd3c2b875c7622985a5e618efb5649
oliver <WANGJIN_1163@163.com>
2023-02-20 14:51:02 +0800

    1.发布动态

--- OUIKit/OUIMoments/src/main/java/io/openim/android/ouimoments/mvp/presenter/PushMomentsVM.java
+++ OUIKit/OUIMoments/src/main/java/io/openim/android/ouimoments/mvp/presenter/PushMomentsVM.java
@@ -1,5 +1,8 @@
 package io.openim.android.ouimoments.mvp.presenter;
 
+import android.os.Build;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
@@ -7,15 +10,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.openim.android.ouicore.base.BaseViewModel;
+import io.openim.android.ouicore.entity.ExUserInfo;
 import io.openim.android.ouimoments.R;
 import io.openim.android.ouimoments.bean.MomentsBean;
 import io.openim.android.ouimoments.bean.MomentsContent;
+import io.openim.android.ouimoments.ui.SelectDataActivity;
 import io.openim.android.sdk.OpenIMClient;
+import io.openim.android.sdk.models.GroupInfo;
 
 public class PushMomentsVM extends BaseViewModel {
     public MutableLiveData<List<Object>> selectMedia = new MutableLiveData<>(new ArrayList<>());
     public MutableLiveData<PushMomentsParam> param = new MutableLiveData<>(new PushMomentsParam());
-
+    //已选择的RuleDataList
+    public List<SelectDataActivity.RuleData> selectedGroupRuleDataList;
+    public List<SelectDataActivity.RuleData> selectedUserRuleDataList;
 
     //是否是发布照片
     boolean isPhoto = true;
@@ -25,11 +33,42 @@ public class PushMomentsVM extends BaseViewModel {
     protected void viewCreate() {
 
     }
-    public void init(){
+
+    public void init() {
         selectMedia.getValue().add(io.openim.android.ouicore.R.mipmap.ic_add3);
         selectMedia.setValue(selectMedia.getValue());
     }
 
+    @NonNull
+    public List<SelectDataActivity.RuleData> buildGroupRuleData(List<GroupInfo> groupInfos
+                                                           ) {
+        List<String> selectedIds = new ArrayList<>();
+        if (null!=selectedGroupRuleDataList){
+            for (SelectDataActivity.RuleData ruleData : selectedGroupRuleDataList) {
+                selectedIds.add(ruleData.id);
+            }
+        }
+        List<SelectDataActivity.RuleData> ruleDataList = new ArrayList<>();
+        for (GroupInfo groupInfo : groupInfos) {
+            SelectDataActivity.RuleData ruleData = new SelectDataActivity.RuleData();
+            ruleData.id = groupInfo.getGroupID();
+            if (selectedIds.contains(groupInfo.getGroupID()))
+                ruleData.isSelect = true;
+            ruleData.name = groupInfo.getGroupName();
+            ruleData.icon = groupInfo.getFaceURL();
+            ruleDataList.add(ruleData);
+        }
+        return ruleDataList;
+    }
+    public String getRuleDataNames(List<SelectDataActivity.RuleData> ruleDataList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (SelectDataActivity.RuleData ruleData : ruleDataList) {
+            stringBuilder.append(ruleData.name);
+            stringBuilder.append("、");
+        }
+        String res = stringBuilder.toString();
+        return res.substring(0, res.length() - 1);
+    }
     /**
      * 添加资源
      *
@@ -72,19 +111,62 @@ public class PushMomentsVM extends BaseViewModel {
     }
 
     public void selectAuth(int index) {
+        selectedUserRuleDataList = null;
+        selectedGroupRuleDataList = null;
+        param.getValue().permissionUserList=null;
+        param.getValue().permissionGroupList=null;
+
         param.getValue().permission = index;
         param.setValue(param.getValue());
     }
 
-    public  static class PushMomentsParam {
+    public void getRuleDataIDs(List<SelectDataActivity.RuleData> ruleDataList, boolean isGroup) {
+        List<UserOrGroup> userOrGroups = new ArrayList<>();
+        for (SelectDataActivity.RuleData ruleData : ruleDataList) {
+            UserOrGroup userOrGroup = new UserOrGroup();
+            if (isGroup) {
+                userOrGroup.groupID = ruleData.id;
+                userOrGroup.groupName = ruleData.name;
+            } else {
+                userOrGroup.userID = ruleData.id;
+                userOrGroup.userName = ruleData.name;
+            }
+            userOrGroups.add(userOrGroup);
+        }
+        if (isGroup) param.getValue().permissionGroupList = userOrGroups;
+        else param.getValue().permissionUserList = userOrGroups;
+    }
+
+    public List<SelectDataActivity.RuleData> buildUserRuleData(List<ExUserInfo> exUserInfo) {
+        List<String> selectedIds = new ArrayList<>();
+        if (null!=selectedUserRuleDataList){
+            for (SelectDataActivity.RuleData ruleData : selectedUserRuleDataList) {
+                selectedIds.add(ruleData.id);
+            }
+        }
+        List<SelectDataActivity.RuleData> ruleDataList = new ArrayList<>();
+        for (ExUserInfo userInfo : exUserInfo) {
+            SelectDataActivity.RuleData ruleData = new SelectDataActivity.RuleData();
+            ruleData.id = userInfo.userInfo.getUserID();
+            if (selectedIds.contains( ruleData.id ))
+                ruleData.isSelect = true;
+            ruleData.name =  userInfo.userInfo.getNickname();
+            ruleData.icon =  userInfo.userInfo.getFaceURL();
+            ruleDataList.add(ruleData);
+        }
+        return ruleDataList;
+    }
+
+    public static class PushMomentsParam {
         public MomentsContent content;
         //0/1/2/3, 公开/私密/部分可见/不给谁看
         public int permission;
         public List<UserOrGroup> permissionUserList;
         public List<UserOrGroup> permissionGroupList;
+        public List<UserOrGroup> atUserList;
     }
 
-    public  static class UserOrGroup {
+    public static class UserOrGroup {
         public String userID;
         public String userName;
         public String groupID;
--- OUIKit/OUIMoments/src/main/java/io/openim/android/ouimoments/ui/PushMomentsActivity.java
+++ OUIKit/OUIMoments/src/main/java/io/openim/android/ouimoments/ui/PushMomentsActivity.java
@@ -1,5 +1,7 @@
 package io.openim.android.ouimoments.ui;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -17,12 +19,17 @@ import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import io.openim.android.ouicore.adapter.RecyclerViewAdapter;
 import io.openim.android.ouicore.base.BaseActivity;
+import io.openim.android.ouicore.entity.ExUserInfo;
 import io.openim.android.ouicore.utils.Common;
+import io.openim.android.ouicore.utils.Constant;
+import io.openim.android.ouicore.utils.OnDedrepClickListener;
+import io.openim.android.ouicore.vm.GroupVM;
 import io.openim.android.ouicore.widget.PhotographAlbumDialog;
 import io.openim.android.ouicore.widget.SpacesItemDecoration;
 import io.openim.android.ouicore.widget.WaitDialog;
@@ -37,10 +44,12 @@ public class PushMomentsActivity extends BaseActivity<PushMomentsVM, ActivityPus
 
     private RecyclerViewAdapter<Object, RoundIMGItem> adapter;
     private PhotographAlbumDialog albumDialog;
+    private WaitDialog waitDialog;
+    GroupVM groupVM = new GroupVM();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        bindVM(PushMomentsVM.class,true);
+        bindVM(PushMomentsVM.class, true);
         super.onCreate(savedInstanceState);
         bindViewDataBinding(ActivityPushMomentsBinding.inflate(getLayoutInflater()));
         sink();
@@ -48,14 +57,53 @@ public class PushMomentsActivity extends BaseActivity<PushMomentsVM, ActivityPus
         listener();
         vm.init();
     }
+    private ActivityResultLauncher<Intent> ruleDataLauncher =
+        registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() != RESULT_OK) return;
+            List<SelectDataActivity.RuleData> selectedRuleDataList =
+                (List<SelectDataActivity.RuleData>) result.getData().getSerializableExtra(Constant.K_RESULT);
+            view.reminderWho.setText(vm.getRuleDataNames(selectedRuleDataList));
+
+            List<PushMomentsVM.UserOrGroup>  userOrGroups=new ArrayList<>();
+            for (SelectDataActivity.RuleData ruleData : selectedRuleDataList) {
+                PushMomentsVM.UserOrGroup userOrGroup=new PushMomentsVM.UserOrGroup();
+                userOrGroup.userID= ruleData.id;
+                userOrGroup.groupName= ruleData.name;
+                userOrGroups.add(userOrGroup);
+            }
+            vm.param.getValue().atUserList=userOrGroups;
+        });
+    private ActivityResultLauncher<Intent> resultLauncher= registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+       if ( result.getResultCode()==RESULT_OK){
+           List<SelectDataActivity.RuleData> ruleDataList =new ArrayList<>();
+           if (null!=vm.selectedGroupRuleDataList)
+           ruleDataList.addAll(vm.selectedGroupRuleDataList);
+           if (null!=vm.selectedUserRuleDataList)
+           ruleDataList.addAll(vm.selectedUserRuleDataList);
+           view.whoSee.setText(vm.getRuleDataNames(ruleDataList));
+       }
+    });
+    private OnDedrepClickListener selectUserClick = new OnDedrepClickListener() {
+        @Override
+        public void click(View v) {
+            if (groupVM.exUserInfo.getValue().isEmpty()) {
+                waitDialog.show();
+                groupVM.getAllFriend();
+            } else jumpSelectUser(groupVM.exUserInfo.getValue());
+        }
+    };
 
     private void listener() {
+        groupVM.exUserInfo.observe(this, exUserInfos -> {
+            waitDialog.dismiss();
+            jumpSelectUser(exUserInfos);
+        });
+
         view.whoSeeLy.setOnClickListener(v -> {
-            startActivity(new Intent(this,WhoSeeActivity.class));
+            resultLauncher.launch(new Intent(this, WhoSeeActivity.class));
         });
-        view.reminderWhoLy.setOnClickListener(v -> {
-            startActivity(new Intent(this,WhoSeeActivity.class));
-        });
+        view.reminderWhoLy.setOnClickListener(selectUserClick);
+
         vm.selectMedia.observe(this, objects -> {
             adapter.setItems(objects);
         });
@@ -73,50 +121,58 @@ public class PushMomentsActivity extends BaseActivity<PushMomentsVM, ActivityPus
 
                 @Override
                 public void afterTextChanged(Editable s) {
-                    view.restriction.setText(s.length()+"/500");
+                    view.restriction.setText(s.length() + "/500");
                 }
             });
     }
 
+    private void jumpSelectUser(List<ExUserInfo> exUserInfo) {
+        if (exUserInfo.isEmpty())return;
+        List<SelectDataActivity.RuleData> ruleDataList = vm.buildUserRuleData(exUserInfo);
+        ruleDataLauncher.launch(new Intent(this, SelectDataActivity.class).putExtra(Constant.K_NAME, getString(io.openim.android.ouicore.R.string.select_user)).putExtra(Constant.K_RESULT, (Serializable) ruleDataList).putExtra(Constant.K_FROM,
+            false).putExtra(Constant.K_SIZE, 20));
+    }
+
     private void initView() {
+        waitDialog = new WaitDialog(this);
         albumDialog = new PhotographAlbumDialog(PushMomentsActivity.this);
         view.recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         view.recyclerView.setAdapter(adapter =
             new RecyclerViewAdapter<Object, RoundIMGItem>(RoundIMGItem.class) {
 
-            @Override
-            public void onBindView(@NonNull RoundIMGItem holder, Object data, int position) {
-                if (data instanceof Integer) {
-                    //表示添加按钮
-                    holder.view.roundImageView.setOnClickListener(v -> {
-                        albumDialog.show();
-                        albumDialog.setMaxSelectable(9);
-                        albumDialog.setToCrop(false);
-                        albumDialog.setOnSelectResultListener(paths -> {
-                            albumDialog.dismiss();
-                            for (String path : paths) {
-                                vm.addRes(path);
-                            }
+                @Override
+                public void onBindView(@NonNull RoundIMGItem holder, Object data, int position) {
+                    if (data instanceof Integer) {
+                        //表示添加按钮
+                        holder.view.roundImageView.setOnClickListener(v -> {
+                            albumDialog.show();
+                            albumDialog.setMaxSelectable(9);
+                            albumDialog.setToCrop(false);
+                            albumDialog.setOnSelectResultListener(paths -> {
+                                albumDialog.dismiss();
+                                for (String path : paths) {
+                                    vm.addRes(path);
+                                }
+                            });
                         });
-                    });
-                    holder.view.delete.setVisibility(View.INVISIBLE);
-                } else {
-                    holder.view.delete.setVisibility(View.VISIBLE);
-                    holder.view.delete.setOnClickListener(v -> {
-                        vm.removeRes(position);
-                    });
-                    holder.view.roundImageView.setOnClickListener(v -> {
-                        List<String> photoUrls = new ArrayList<>();
-                        for (Object o : vm.selectMedia.getValue()) {
-                            if (o instanceof String) photoUrls.add((String) o);
-                        }
-                        ImagePagerActivity.startImagePagerActivity(PushMomentsActivity.this,
-                            photoUrls, position, null);
-                    });
+                        holder.view.delete.setVisibility(View.INVISIBLE);
+                    } else {
+                        holder.view.delete.setVisibility(View.VISIBLE);
+                        holder.view.delete.setOnClickListener(v -> {
+                            vm.removeRes(position);
+                        });
+                        holder.view.roundImageView.setOnClickListener(v -> {
+                            List<String> photoUrls = new ArrayList<>();
+                            for (Object o : vm.selectMedia.getValue()) {
+                                if (o instanceof String) photoUrls.add((String) o);
+                            }
+                            ImagePagerActivity.startImagePagerActivity(PushMomentsActivity.this,
+                                photoUrls, position, null);
+                        });
+                    }
+                    Glide.with(PushMomentsActivity.this).load(data).centerInside().into(holder.view.roundImageView);
                 }
-                Glide.with(PushMomentsActivity.this).load(data).centerInside().into(holder.view.roundImageView);
-            }
-        });
+            });
         adapter.setItems(vm.selectMedia.getValue());
     }
 
